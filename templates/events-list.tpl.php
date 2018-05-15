82ab3779c2e84eeaedf759742dc01d7ea6f250fc
Mihaela Stavila <ms@inleadmedia.com>
2018-05-15 15:40:32 +0300

    EASYOPAC-866 - Change template for event calendar.

--- templates/events-list.tpl.php
+++ templates/events-list.tpl.php
@@ -21,18 +21,20 @@
       $raw_category = field_view_field('node', $item, 'field_ding_event_category', array('label' => 'hidden'));
       $raw_location = field_view_field('node', $item, 'og_group_ref', array('label' => 'hidden'));
       ?>
-      <div class="event-time"><?php print $event_time; ?></div>
       <div class="event-title">
         <a href="<?php print url('node/' . $item->nid); ?>">
           <?php print $item->title; ?>
         </a>
       </div>
+      <div class="info-event">
       <?php if (!empty($raw_location)) : ?>
-        <div class="event-location"><?php print l($raw_location[0]['#markup'], 'node/' . $raw_location['#items'][0]['target_id']); ?></div>
-      <?php endif; ?>
-      <?php if (!empty($raw_category)) : ?>
-        <div class="event-category"><?php print render($raw_category); ?></div>
-      <?php endif; ?>
+          <div class="event-location"><?php print l($raw_location[0]['#markup'], 'node/' . $raw_location['#items'][0]['target_id']); ?></div>
+        <?php endif; ?>
+        <div class="event-time"> <span><?php print $event_time; ?></span></div>
+        <?php if (!empty($raw_category)) : ?>
+          <div class="event-category"><?php print render($raw_category); ?></div>
+        <?php endif; ?>
+      </div>
     </div>
   <?php endforeach; ?>
 </div>
